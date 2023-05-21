bb59a622451da03b33dfe6f59db498b502f9002d
Hamberfim <adhamlingithub@gmail.com>
2023-05-21 09:20:44 -0500

    re-structure

---
+++ 03_LanguageCore/DiffOperators3_3ArrObj.js
@@ -0,0 +1,50 @@
+// optional chaining operator and accessing an array nested object properties
+
+const staff = [
+  {
+    firstName: 'john',
+    lastName: 'doe',
+    contact: { email: 'jdoe@fake.mail', phone: '555-555-1001' },
+  },
+  {
+    firstName: 'mary',
+    lastName: 'doyle',
+    contact: { email: 'mdoyle@fake.mail' },
+  },
+  {
+    firstName: 'bill',
+    lastName: 'west',
+    contact: { email: 'bwest@fake.mail', phone: '555-555-1003' },
+  },
+  {
+    firstName: 'sammy',
+    lastName: 'smith',
+    contact: { email: 'ssmith@fake.mail' },
+  },
+  {
+    firstName: 'tom',
+    lastName: 'jones',
+    contact: { email: 'tjones@fake.mail', phone: '555-555-1005' },
+  },
+];
+
+// access nested properties via optional chaining operator
+console.log(staff[0].firstName, staff[0].contact?.phone, '\n');
+
+// nested properties via optional chaining operator as a function
+function getStaffContactPhone(arr, fName) {
+  let contact = `${fName} not found`;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].firstName == fName) {
+      if (arr[i].contact.phone) {
+        contact = `${fName}: ${arr[i].contact?.phone}`;
+      } else {
+        contact = `${fName}: has no phone contact`;
+      }
+    }
+  }
+  console.log(contact);
+}
+getStaffContactPhone(staff, 'john');
+getStaffContactPhone(staff, 'sammy');
+getStaffContactPhone(staff, 'wanda');
---
+++ 03_LanguageCore/DiffOperators3_3ObjObjs.js
@@ -0,0 +1,52 @@
+// optional chaining operator and accessing nested object properties
+
+const staff = {
+  1001: {
+    firstName: 'john',
+    lastName: 'doe',
+    contact: { email: 'jdoe@fake.mail', phone: '555-555-1001' },
+  },
+  1002: {
+    firstName: 'mary',
+    lastName: 'doyle',
+    contact: { email: 'mdoyle@fake.mail' },
+  },
+  1003: {
+    firstName: 'bill',
+    lastName: 'west',
+    contact: { email: 'bwest@fake.mail', phone: '555-555-1003' },
+  },
+  1004: {
+    firstName: 'sammy',
+    lastName: 'smith',
+    contact: { email: 'ssmith@fake.mail' },
+  },
+  1005: {
+    firstName: 'tom',
+    lastName: 'jones',
+    contact: { email: 'tjones@fake.mail', phone: '555-555-1005' },
+  },
+};
+
+// access nested properties via optional chaining operator
+console.log(staff['1001'].firstName, staff['1001'].contact?.phone, '\n');
+
+// nested properties via optional chaining operator as a function
+function getStaffContactPhoneById(arr, id) {
+  let contact = `${id} not found`;
+  for (const key in arr) {
+    //console.log(key, id);
+    if (key === id) {
+      if (arr[key].contact.phone) {
+        contact = `${key} ${arr[key].firstName}: ${arr[key].contact?.phone}`;
+      } else {
+        contact = `${id} : Has no phone contact`;
+      }
+    }
+  }
+  console.log(contact);
+}
+
+getStaffContactPhoneById(staff, '1001');
+getStaffContactPhoneById(staff, '1004');
+getStaffContactPhoneById(staff, '2000');
