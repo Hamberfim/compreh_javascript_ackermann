20b077f5a8946a67cab257c25e531612e4aa7352
Hamberfim <adhamlingithub@gmail.com>
2023-08-12 11:51:26 -0500

    sec 3.6.6, p206

--- 03_LanguageCore/src/tryCatch3_6_6.mjs
+++ 03_LanguageCore/src/tryCatch3_6_6.mjs
@@ -2,28 +2,38 @@
 /**
  * @param {string | number} age
  */
+// function checkAge(age) {
+//   if (typeof age === 'string') {
+//     age = parseInt(age);
+//     if (isNaN(age)) {
+//       throw new Error(`input must be a number`);
+//     } else {
+//       if (age < 0) {
+//         throw new Error('Age must be a positive number.');
+//       } else {
+//         return true;
+//       }
+//     }
+//   } else if (typeof age === 'number') {
+//     if (age < 0) {
+//       throw new Error('Age must be a positive number.');
+//     } else {
+//       return true;
+//     }
+//   }
+// }
+
+// refactor
 function checkAge(age) {
-  if (typeof age === 'string') {
-    age = parseInt(age);
-    if (isNaN(age)) {
-      throw new Error(`input must be a number`);
-    } else {
-      if (age < 0) {
-        throw new Error('Age must be a positive number.');
-      } else {
-        return true;
-      }
-    }
-  } else if (typeof age === 'number') {
-    if (age < 0) {
-      throw new Error('Age must be a positive number.');
-    } else {
-      return true;
-    }
+  if (isNaN(parseFloat(age))) {
+    throw new Error(`input must be a number`);
+  } else if (age < 0) {
+    throw new Error('Age must be a positive number.');
   }
+  return true;
 }
 
 console.log(checkAge(22)); // true
 console.log(checkAge(0)); // true
-console.log(checkAge('22')); // error
+console.log(checkAge('36')); // true
 console.log(checkAge('pizza')); // error
