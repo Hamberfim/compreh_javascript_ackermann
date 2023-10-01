c128785df71ec5b90e97e5aaf915dfc58ffbac93
Hamberfim <adhamlingithub@gmail.com>
2023-10-01 11:24:42 -0500

    Object helper methods .keys()/.values()

--- 04_refTypes/objects_4.2.9.mjs
+++ 04_refTypes/objects_4.2.9.mjs
@@ -1,5 +1,5 @@
 'use strict';
-// define item object, it's properties and property descriptors
+// define item Object, it's properties and property descriptors
 const item = {};
 Object.defineProperties(item, {
   make: {
@@ -38,17 +38,33 @@ Object.defineProperties(item, {
       console.log(`A ${item.year} ${item.color} ${item.make} ${item.model} for $${item.price}\n`);
     },
     writable: false,
+    // not enumerable
     enumerable: false,
     configurable: false,
   },
 });
 
 item.displayItemDescription();
-// outputting the objects properties and property descriptors via for-in loop
+console.log('--- Using a for-in loop ---');
+// outputting the Objects properties and property descriptors via for-in loop
 for (let property in item) {
   let propertyDescriptor = Object.getOwnPropertyDescriptor(item, property);
   console.log(`Name: ${property}, Value: ${item[property]}`);
   console.log(` ---- Writable: ${propertyDescriptor.writable}`);
   console.log(` ---- Enumerable: ${propertyDescriptor.enumerable}`);
   console.log(` ---- Configurable: ${propertyDescriptor.configurable}\n`);
+}
+
+// outputting the Objects properties and property descriptors via helper methods
+console.log('--- Using Object .keys/.values helper methods and a for loop ---');
+const itemsKeys = Object.keys(item);
+const itemValues = Object.values(item);
+for (let i = 0; i < itemsKeys.length; i++) {
+  const propertyName = itemsKeys[i];
+  const propertyValue = itemValues[i];
+  const propertyDescriptor = Object.getOwnPropertyDescriptor(item, itemsKeys[i]);
+  console.log(`Name: ${propertyName}, Value: ${propertyValue}`);
+  console.log(` .... Writable: ${propertyDescriptor.writable}`);
+  console.log(` .... Enumerable: ${propertyDescriptor.enumerable}`);
+  console.log(` .... Configurable: ${propertyDescriptor.configurable}\n`);
 }
