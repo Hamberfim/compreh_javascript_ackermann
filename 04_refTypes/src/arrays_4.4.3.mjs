5b31760625ccfa13fcb9bd9209302efbaece01a7
Hamberfim <adhamlingithub@gmail.com>
2023-10-17 13:41:59 -0500

    destructuring in a loop

--- 04_refTypes/src/arrays_4.4.3.mjs
+++ 04_refTypes/src/arrays_4.4.3.mjs
@@ -26,3 +26,24 @@ for (let {
 } of persons) {
   console.log(`${firstName} ${lastName}, ${email}, ${phone}`);
 }
+
+// extracting args of a function
+function displayPerson({ firstName: fn, lastName: ln, contact: { email: e, phone: p } }) {
+  console.log(`${fn} ${ln}, ${e}, ${p}`);
+}
+// extracting some args of a function
+function contactInfoOnly({ contact: { email: e, phone: p } }) {
+  console.log(`Contact Info: ${e} | ${p}`);
+}
+
+const dave = {
+  firstName: "Dave",
+  lastName: "Doe",
+  contact: {
+    email: "dave@fake.org",
+    phone: "555-555-3333",
+  },
+};
+// @ts-ignore
+displayPerson(dave);
+contactInfoOnly(dave);
