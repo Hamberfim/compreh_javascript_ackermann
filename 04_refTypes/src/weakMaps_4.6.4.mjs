3c36b7ae6e1531bb6dec462165b0a9b58402cbec
Hamberfim <adhamlingithub@gmail.com>
2023-10-24 10:35:34 -0500

    4.9

--- 04_refTypes/src/weakMaps_4.6.4.mjs
+++ 04_refTypes/src/weakMaps_4.6.4.mjs
@@ -40,4 +40,41 @@ const topArtists = new WeakSet([artist1, artist2, artist3, artist4, artist5]);
 console.log(topArtists);
 console.log(topArtists.has(artist1));
 
-// other global objects - 4.8 - Date/Time
+// wrapper object for primitive types
+const theNumberThree = new Number(3);
+console.log(theNumberThree);
+console.log(theNumberThree.toExponential());
+console.log(theNumberThree.toPrecision(5)); // total display including number
+console.log(theNumberThree.toFixed(2)); // addition decimal positions
+
+// regular expressions - 4.9
+const regExpConstruct = new RegExp("abcd"); // via constructor - input as a string
+const regExpLiteralNotation = /abcd/; // via literal notation - faster than the constructor
+// test - does the specific 'string' character pattern occur
+console.log(regExpConstruct.test("abcdefg")); // true
+console.log(regExpLiteralNotation.test("gfedcba")); // false
+console.log(regExpLiteralNotation.test("abcdefg|gfedcba")); // does 'abcdefg' OR 'gfedcba' occur
+let myRegExp = /...../; // at least any five string characters
+console.log(myRegExp.test("497")); // false
+console.log(myRegExp.test("49752")); // true
+console.log(myRegExp.test("49752111")); // true
+console.log(myRegExp.test("497  111")); // true - spaces count
+
+// regEx character classes
+myRegExp = /[abcde]/; // simple class
+// contains on letter in one place
+console.log(myRegExp.test("l")); // false
+console.log(myRegExp.test("ghij")); // false
+console.log(myRegExp.test("acd")); // true
+console.log(myRegExp.test("cxyz")); // true
+
+// range class
+console.log(myRegExp.test("A-Z")); //  false no cap letters
+const localLongPhoneNum = /[1].[0-9][0-9][0-9].[0-9][0-9][0-9].[0-9][0-9][0-9][0-9]/; // #-###-###-####
+console.log(localLongPhoneNum.test("1-515-967-1111")); // true
+console.log(localLongPhoneNum.test("1 515 967 1111")); // true
+console.log(localLongPhoneNum.test("3 515 967 1111")); // false
+console.log(localLongPhoneNum.test("515-967-1111")); // false
+const localPhoneNum = /[0-9][0-9][0-9].[0-9][0-9][0-9][0-9]/; // ###-####
+console.log(localPhoneNum.test("515 967 1111")); // true
+console.log(localPhoneNum.test("967-1111")); // true
