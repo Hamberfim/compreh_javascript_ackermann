b9e2092bf5f34584f4a1578202958993ca5f0bd2
Hamberfim <adhamlingithub@gmail.com>
2023-10-24 09:44:23 -0500

    4.7. - 4.8

--- 04_refTypes/src/weakMaps_4.6.4.mjs
+++ 04_refTypes/src/weakMaps_4.6.4.mjs
@@ -19,6 +19,25 @@ studioAlbums.set(artist2, 8);
 studioAlbums.set(artist3, 4);
 studioAlbums.set(artist4, 12);
 studioAlbums.set(artist5, 29);
+console.log(studioAlbums);
 console.log(studioAlbums.has(artist1)); // true
 console.log(studioAlbums.get(artist1)); // 10
 studioAlbums.delete(artist5);
+
+// Using sets 4.7  a data structure similar to a list
+const artists = new Set(["David Byrne", "Talking Heads", "Smiths", "Pretenders", "Brian Eno"]);
+console.log(artists.size);
+// Set methods
+artists.add("Tom Tom Club");
+console.log(artists.has("Brian Eno"));
+console.log(artists.size);
+for (const artist of artists) {
+  console.log(artist);
+}
+
+// WeakSet - similar to WeakMap
+const topArtists = new WeakSet([artist1, artist2, artist3, artist4, artist5]);
+console.log(topArtists);
+console.log(topArtists.has(artist1));
+
+// other global objects - 4.8 - Date/Time
