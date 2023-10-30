2d1bc00907f20c94aa7655b07da9be761c579a0f
Hamberfim <adhamlingithub@gmail.com>
2023-10-30 14:24:44 -0500

    sec 5.2

---
+++ 05_DOM/index_5.2.js
@@ -0,0 +1,6 @@
+/* Selecting elements from the Document Object Model (DOM). */
+"use strict";
+
+// assign the main tag element the bootstrap 5 class of container-fluid and m-1
+const mainElement = document.querySelector("main");
+mainElement.className = "container-fluid m-1";
