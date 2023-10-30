d730bbf14d2ffbfb68f3c5f23c277443629188a3
Hamberfim <adhamlingithub@gmail.com>
2023-10-30 14:42:10 -0500

    sec 5.2 index setup

--- 05_DOM/index_5.2.js
+++ 05_DOM/index_5.2.js
@@ -1,6 +1,14 @@
 /* Selecting elements from the Document Object Model (DOM). */
 "use strict";
 
-// assign the main tag element the bootstrap 5 class of container-fluid and m-1
+// select the primary semantic tags (header, nav, main, footer) and assign bootstrap 5 classes to them
+const headerElement = document.querySelector("header");
+headerElement.className = "container-fluid m-1";
+const navElement = document.querySelector("nav");
+navElement.className = "container-fluid m-1";
 const mainElement = document.querySelector("main");
 mainElement.className = "container-fluid m-1";
+const footerElement = document.querySelector("footer");
+footerElement.className = "container-fluid m-1";
+
+// section and articles
