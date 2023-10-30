385edcb29f5ee374aca08bce174cf5eb9804f613
Hamberfim <adhamlingithub@gmail.com>
2023-10-30 16:25:46 -0500

    sec 5.2 index setup

--- 05_DOM/index_5.2.js
+++ 05_DOM/index_5.2.js
@@ -1,14 +1,22 @@
 /* Selecting elements from the Document Object Model (DOM). */
 "use strict";
 
-// select the primary semantic tags (header, nav, main, footer) and assign bootstrap 5 classes to them
+// Select the primary semantic tags (header, nav, main, footer) and assign bootstrap 5 classes to them
 const headerElement = document.querySelector("header");
-headerElement.className = "container-fluid m-1";
 const navElement = document.querySelector("nav");
-navElement.className = "container-fluid m-1";
 const mainElement = document.querySelector("main");
-mainElement.className = "container-fluid m-1";
 const footerElement = document.querySelector("footer");
-footerElement.className = "container-fluid m-1";
+if (headerElement !== null && navElement !== null && mainElement !== null && footerElement !== null) {
+  headerElement.className = "container-fluid m-1";
+  navElement.className = "container-fluid m-1";
+  mainElement.className = "container-fluid m-1";
+  footerElement.className = "container-fluid m-1";
+}
 
 // section and articles
+/** querySelectorAll() method returns a NodeList object, (which is similar to an array),
+ * containing all the elements that match the specified CSS selector. */
+const sectionElements = document.querySelectorAll("section");
+sectionElements.forEach((key) => {
+  key.className = "border border-dark m-2 ps-2 p-1";
+});
